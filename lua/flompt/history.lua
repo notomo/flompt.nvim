7cf7beae2d8c7b7ba7e95f29451e26c40b012ef5
notomo <notomo.motono@gmail.com>
2021-02-12 22:35:43 +0900

    Refactor history

--- lua/flompt/history.lua
+++ lua/flompt/history.lua
@@ -2,11 +2,11 @@ local M = {}
 
 function M.load(bufnr)
   local lines
-  local shell = vim.o.shell
-  if shell == "zsh" then
-    lines = M._zsh()
-  elseif shell == "bash" then
-    lines = M._bash()
+  local exe = vim.o.shell
+  if M._has("zsh", exe) then
+    lines = M._zsh(exe)
+  elseif M._has("bash", exe) then
+    lines = M._bash(exe)
   end
   if lines == nil then
     return
@@ -14,30 +14,38 @@ function M.load(bufnr)
   vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
 end
 
-function M._zsh()
-  local path = vim.fn.systemlist({vim.o.shell, "-i", "-c", "echo $HISTFILE"})[1]
+function M._has(name, exe)
+  return exe == name or vim.endswith(exe, "/" .. name)
+end
+
+function M._zsh(exe)
+  local path = vim.fn.systemlist({exe, "-i", "-c", "echo ${HISTFILE}"})[1]
   if path == nil then
     return
   end
+
   local f = io.open(path, "r")
   if f == nil then
     return
   end
+
   local lines = vim.split(f:read("*a"), "\n", true)
   return vim.tbl_map(function(line)
     return line:gsub(".*;", "")
   end, lines)
 end
 
-function M._bash()
-  local path = vim.fn.systemlist({"bash", "-c", "echo ${HISTFILE}"})[1]
+function M._bash(exe)
+  local path = vim.fn.systemlist({exe, "-c", "echo ${HISTFILE}"})[1]
   if path == nil then
     return
   end
+
   local f = io.open(path, "r")
   if f == nil then
     return
   end
+
   return vim.split(f:read("*a"), "\n", true)
 end
 
